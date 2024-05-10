7c78be45eda46a170d5c55d3bae8594cafb0a13e
Matthew Xavier Hernandez <77997996+MegaMatt705@users.noreply.github.com>
2024-05-10 13:52:36 -0700

    Update vecadd_cpu.cpp
    reverting change

--- vecadd_cpu.cpp
+++ vecadd_cpu.cpp
@@ -2,9 +2,9 @@
 #include <math.h>
 #include <chrono>
 
-// CUDA Kernel function to add the elements of two arrays on the GPU
-__global__
-void add(int n, float *x, float *y) {
+// function to add the elements of two arrays
+void add(int n, float *x, float *y)
+{
   for (int i = 0; i < n; i++)
       y[i] = x[i] + y[i];
 }
